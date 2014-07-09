7fe2b4697d44af576a08580b533cc223a02e99d2
John Arley Burns <johnarleyburns@gmail.com>
2014-07-08 19:38:38 -0500

    Initial Commit

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by John Arley Burns on 08/07/14.
+//  Copyright (c) 2014 John Arley Burns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
